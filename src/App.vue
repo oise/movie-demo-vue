758766af2386aa3deba620eb1378653b78f560c4
Oise Ojeikere <hello@oise.me>
2021-05-11 21:19:32 +0100

    feat: Initial app setup

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <Home/>
+</template>
+
+<script lang="ts">
+  import Home from "./components/Home/Home.vue";
+
+  export default {
+    name: "App",
+    components: {
+      Home
+    }
+  };
+</script>
+
+<style lang="scss">
+  @import "./shared/scss/styles";
+</style>
